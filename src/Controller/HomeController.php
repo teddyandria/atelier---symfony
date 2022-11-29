b2b2f0b458eab012ec0426b4b014b706b896540e
teddyandria <teddy.andriamam@gmail.com>
2022-11-29 16:17:14 +0100

    show squirrels

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\DataFixtures\SquirrelFixtures;
+use App\Repository\SquirrelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/home', name: 'home_index')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(SquirrelRepository $squirrels): Response
     {
-        return $this->render('home/index.html.twig');
+
+        $squirrelView = $squirrels->findAll();
+        return $this->render('home/index.html.twig', [
+            'squirrels' => $squirrelView,
+        ]);
     }
 }
---
+++ src/DataFixtures/SquirrelFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Squirrel;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class SquirrelFixtures extends Fixture
+{
+
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $squirrel = new Squirrel();
+            $squirrel->setName('name' . $i);
+            $manager->persist($squirrel);
+        }
+        $manager->flush();
+    }
+}
